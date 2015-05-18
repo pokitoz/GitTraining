78ca7ceb886651888058d053337f8bd118438a82
pokitoz <florian.depraz@epfl.ch>
2015-05-18 13:37:10 +0200

    Renaming

--- main.c
+++ main.c
@@ -5,9 +5,9 @@ int main(void)
 printf("Branch1!");
 return 0;
 }
-void fun_a(void){}
-void fun_b(void){}
-void fun_c(void){}
-void fun_d(void){}
-void fun_e(void){}
-void fun_f(void){}
+void fun_n(void){}
+void fun_m(void){}
+void fun_l(void){}
+void fun_p(void){}
+void fun_q(void){}
+void fun_r(void){}
