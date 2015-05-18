c32ecebff18d4d216fcd1fa237af781efbd294fc
pokitoz <florian.depraz@epfl.ch>
2015-05-18 13:50:24 +0200

    Automatic commit function az

--- main.c
+++ main.c
@@ -5,9 +5,14 @@ int main(void)
 printf("Branch1!");
 return 0;
 }
+<<<<<<< HEAD
 void fun_n(void){}
 void fun_m(void){}
 void fun_l(void){}
 void fun_p(void){}
 void fun_q(void){}
 void fun_r(void){}
+=======
+void fun_a(void){}
+>>>>>>> 48ae9fd... Automatic commit function a
+void fun_az(void){}
